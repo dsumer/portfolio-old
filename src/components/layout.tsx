6a06598dcaefe9a12734b4eafc3a82241fe13c19
dsumer <domi.sumer@gmail.com>
2020-12-03 20:46:23 +0100

    added open graph meta tags

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -20,6 +20,14 @@ const Layout = (props: PropsWithChildren<unknown>) => {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🙌</text></svg>"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://dominik.sumer.dev/" />
+        <meta property="og:site_name" content="Dominik Sumer - Web Developer" />
+        <meta
+          property="og:image"
+          itemProp="image primaryImageOfPage"
+          content="https://dominik.sumer.dev/images/profile-placeholder.png"
+        />
       </Head>
       <CSSReset />
       <Global
