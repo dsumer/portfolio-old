215e2ad50ee8569564e1ca75485ff838c24d17cb
dsumer <domi.sumer@gmail.com>
2020-12-15 17:26:51 +0100

    using vercel rewrites instead of nextjs

--- next.config.js
+++ next.config.js
@@ -3,16 +3,4 @@ module.exports = {
     locales: ['en'],
     defaultLocale: 'en',
   },
-  rewrites: async () => {
-    return [
-      {
-        source: '/bee.js',
-        destination: 'https://cdn.splitbee.io/sb.js',
-      },
-      {
-        source: '/_hive/:slug',
-        destination: 'https://hive.splitbee.io/:slug',
-      },
-    ];
-  },
 };
