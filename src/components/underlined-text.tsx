45b1ef58aceed58695654b6dbbf69a84d8480600
dsumer <domi.sumer@gmail.com>
2021-01-06 00:14:29 +0100

    tbh wtf

--- src/components/underlined-text.tsx
+++ src/components/underlined-text.tsx
@@ -9,16 +9,7 @@ interface Props {
 const UnderlinedText = (props: PropsWithChildren<Props>) => (
   <Box as="span" display="inline-block" position="relative">
     {props.children}
-    <Box
-      as="span"
-      display="block"
-      position="absolute"
-      bg={props.color}
-      w={'100%'}
-      h={props.h || '4px'}
-      bottom={0}
-      zIndex={typeof props.zIndex === 'undefined' ? -1 : props.zIndex}
-    />
+    <Box as="span" display="block" position="absolute" bg={props.color} w={'100%'} h={props.h || '4px'} bottom={0} />
   </Box>
 );
 export default UnderlinedText;
