ded91ba59a5499e9a2d00d080b42375d79f499af
dsumer <domi.sumer@gmail.com>
2021-01-05 22:49:01 +0100

    removed initialcolormode theme

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -9,9 +9,6 @@ const ButtonNavVariant = (props: ButtonProps) => ({
 });
 
 const customTheme = extendTheme({
-  config: {
-    initialColorMode: 'system',
-  },
   styles: {
     global: {
       html: {
