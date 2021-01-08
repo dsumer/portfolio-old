c82b80705625e3f5520b89310a7be773cf291481
dsumer <domi.sumer@gmail.com>
2021-01-09 00:53:52 +0100

    lambda test

--- src/utils/repng.js
+++ src/utils/repng.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-const chrome = require('chrome-aws-lambda');
+// const chrome = require('chrome-aws-lambda');
 /*const { createElement: h } = require('react');
 const { renderToStaticMarkup } = require('react-dom/server');*/
 
